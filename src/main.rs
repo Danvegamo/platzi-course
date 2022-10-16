402f2ddfa02f13057615149e395d99647ce85715
lmny-xr <d_vega@lmny.art>
2022-10-16 02:14:16 -0500

    WHEATER IN BOGOTA

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+   let name = "Wheater in Bogota";
+   let name1 = "minimum temperature";
+   let name2 = "maximum temperature";
+   let min_temp: i8 = -6;
+   let max_temp: u8 = 25;
+
+    println!("The {} was have a {} of{} and {} of {}.", name, name1, min_temp, name2, max_temp);
+}
